e588b84abe92506f257b0f89af382322c3fec97f
Hans Wage <hans@hanswage.nl>
2023-02-06 18:05:06 +0100

    Setup startup project

---
+++ easy repo search/easy_repo_searchApp.swift
@@ -0,0 +1,17 @@
+//
+//  easy_repo_searchApp.swift
+//  easy repo search
+//
+//  Created by Hans Wage on 06/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct easy_repo_searchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
